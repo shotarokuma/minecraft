87eeb9b8ff5116249650938ffe315e6ed1ed4b4c
Shotaro Kumagai <75811152+shotarokuma@users.noreply.github.com>
2022-07-22 02:07:49 -0700

    create Table

--- docker/init/sql/init-data.sql
+++ docker/init/sql/init-data.sql
@@ -1,3 +1,142 @@
-insert into user(id, name)values(1, 'test1');
-insert into user(id, name)values(2, 'test2');
-insert into user(id, name)values(3, 'test3');
+INSERT INTO Chunks VALUES(0,0,0, 'rain');
+INSERT INTO Chunks VALUES(0,0,1, 'sunny');
+INSERT INTO Chunks VALUES(0,0,2, 'sunny');
+INSERT INTO Chunks VALUES(1,0,0, 'rain');
+INSERT INTO Chunks VALUES(0,1,0, 'rain');
+
+INSERT INTO Block_Types VALUES('Stone', 4, 'Rock');
+INSERT INTO Block_Types VALUES('Dirt', 1, 'Ground');
+INSERT INTO Block_Types VALUES('Cobblestone', 4, 'Rock');
+INSERT INTO Block_Types VALUES('Gold', 12, 'Metal');
+INSERT INTO Block_Types VALUES('Ancient Debris', 9, 'Rock');
+
+INSERT INTO Blocks VALUES(21,15,26, 0,0,0, 'Stone');
+INSERT INTO Blocks VALUES(17,10,9, 0,0,1, 'Dirt');
+INSERT INTO Blocks VALUES(170,55,14, 0,0,2, 'Cobblestone');
+INSERT INTO Blocks VALUES(1,20,94, 1,0,0, 'Gold');
+INSERT INTO Blocks VALUES(76,22,39, 0,1,0, 'Ancient Debris');
+
+INSERT INTO Items(Type) VALUES('Boat');
+INSERT INTO Items(Type) VALUES('Pickaxe');
+INSERT INTO Items(Type) VALUES('Sword');
+INSERT INTO Items(Type) VALUES('Heart of the Sea');
+INSERT INTO Items(Type) VALUES('Spyglass');
+INSERT INTO Items(Type) VALUES('Trident');
+INSERT INTO Items(Type) VALUES('Fishing Rod');
+INSERT INTO Items(Type) VALUES('Helmet');
+
+INSERT INTO Tools VALUES(1, 10);
+INSERT INTO Tools VALUES(2, 22);
+INSERT INTO Tools VALUES(5, 12);
+INSERT INTO Tools VALUES(6, 10);
+INSERT INTO Tools VALUES(7, 11);
+
+INSERT INTO Enchantments VALUES('Silk Touch', 1);
+INSERT INTO Enchantments VALUES('Mending', 1);
+INSERT INTO Enchantments VALUES('Unbreaking', 2);
+INSERT INTO Enchantments VALUES('Aqua Affinity', 2);
+INSERT INTO Enchantments VALUES('Feather Falling', 3);
+
+INSERT INTO AppliedTo VALUES('Silk Touch', 1, 1);
+INSERT INTO AppliedTo VALUES('Mending', 1, 1);
+INSERT INTO AppliedTo VALUES('Unbreaking', 5, 2);
+INSERT INTO AppliedTo VALUES('Unbreaking', 4, 2);
+INSERT INTO AppliedTo VALUES('Mending', 7, 1);
+
+INSERT INTO User VALUES(1,'Mark',2,0,2, 1, 'Hunger',1,2,3);
+INSERT INTO User VALUES(2,'Bob', 9,0,2, 5, 'Death',2,3,4);
+INSERT INTO User VALUES(3,'Catherine', 0,1,3, 5, 'Instant Health',1,4,5);
+INSERT INTO User VALUES(4,'Brian', 3,4,5, 2, 'Absorption',2,4,5);
+INSERT INTO User VALUES(5,'Hao', 1,3,5, 2, 'Death',1,4,5);
+
+
+INSERT INTO Villages VALUES(1,0,2);
+INSERT INTO Villages VALUES(4,0,2);
+INSERT INTO Villages VALUES(5,6,8);
+INSERT INTO Villages VALUES(1,3,5);
+INSERT INTO Villages VALUES(3,5,1);
+
+INSERT INTO Villagers VALUES(1,1,0,2, 'Armourer', 'Death');
+INSERT INTO Villagers VALUES(3,4,0,2, 'Farmer', 'Hunger');
+INSERT INTO Villagers VALUES(2,5,6,8, 'Fisherman', 'Death');
+INSERT INTO Villagers VALUES(4,3,5,1, 'Cartographer', 'Death');
+INSERT INTO Villagers VALUES(5,5,6,8, 'Leatherworker', 'Instat Health');
+
+INSERT INTO Host_In VALUES('bad',3,5,1, 1);
+INSERT INTO Host_In VALUES('good',4,0,2, 2);
+INSERT INTO Host_In VALUES('good',1,3,5, 3);
+INSERT INTO Host_In VALUES('bad', 3,5,1, 4);
+
+INSERT INTO Storage(Type) VALUES('User_storage');
+INSERT INTO Storage(Type) VALUES('User_storage');
+INSERT INTO Storage(Type) VALUES('User_storage');
+INSERT INTO Storage(Type) VALUES('User_storage');
+INSERT INTO Storage(Type) VALUES('User_storage');
+INSERT INTO Storage(Type) VALUES('Ender_storage');
+INSERT INTO Storage(Type) VALUES('Ender_storage');
+INSERT INTO Storage(Type) VALUES('Ender_storage');
+INSERT INTO Storage(Type) VALUES('Chest');
+INSERT INTO Storage(Type) VALUES('Chest');
+INSERT INTO Storage(Type) VALUES('Chest');
+
+INSERT INTO Items_stored_at VALUES(1, 2, 1);
+INSERT INTO Items_stored_at VALUES(1, 1, 1);
+INSERT INTO Items_stored_at VALUES(1, 3, 1);
+INSERT INTO Items_stored_at VALUES(1, 4, 1);
+INSERT INTO Items_stored_at VALUES(2, 1, 2);
+INSERT INTO Items_stored_at VALUES(2, 6, 1);
+INSERT INTO Items_stored_at VALUES(2, 7, 1);
+INSERT INTO Items_stored_at VALUES(3, 1, 3);
+INSERT INTO Items_stored_at VALUES(6, 2, 2);
+INSERT INTO Items_stored_at VALUES(7, 3, 2);
+INSERT INTO Items_stored_at VALUES(9, 5, 1);
+INSERT INTO Items_stored_at VALUES(10, 6, 2);
+
+INSERT INTO Blocks_stored_at VALUES(1, 1, 'Stone');
+INSERT INTO Blocks_stored_at VALUES(1, 2, 'Dirt');
+INSERT INTO Blocks_stored_at VALUES(1, 5, 'CobbleStone');
+INSERT INTO Blocks_stored_at VALUES(2, 5, 'Stone');
+INSERT INTO Blocks_stored_at VALUES(2, 3, 'Gold');
+INSERT INTO Blocks_stored_at VALUES(3, 2, 'Stone');
+INSERT INTO Blocks_stored_at VALUES(3, 3, 'Dirt');
+INSERT INTO Blocks_stored_at VALUES(5, 3, 'Stone');
+INSERT INTO Blocks_stored_at VALUES(8, 4, 'Gold');
+INSERT INTO Blocks_stored_at VALUES(11, 3, 'Ancient Debris');
+
+INSERT INTO Chest VALUES(9, 22,33,44);
+INSERT INTO Chest VALUES(10, 64,58,88);
+INSERT INTO Chest VALUES(11, 45,56,77);
+
+INSERT INTO Ender_Chest VALUES(6, 1);
+INSERT INTO Ender_Chest VALUES(7, 3);
+INSERT INTO Ender_Chest VALUES(8, 4);
+
+INSERT INTO User_storage VALUES(1, 1);
+INSERT INTO User_storage VALUES(2, 2);
+INSERT INTO User_storage VALUES(3, 3);
+INSERT INTO User_storage VALUES(4, 4);
+INSERT INTO User_storage VALUES(5, 5);
+
+INSERT INTO Monster VALUES(1, 'Hunger', 1);
+INSERT INTO Monster VALUES(2, 'Death', 2);
+INSERT INTO Monster VALUES(3, 'Normal', 5);
+INSERT INTO Monster VALUES(4, 'Hunger', 4);
+INSERT INTO Monster VALUES(5, 'Death', 2);
+
+INSERT INTO Gangs VALUES('Village');
+INSERT INTO Gangs VALUES('Desert');
+INSERT INTO Gangs VALUES('Mountain');
+INSERT INTO Gangs VALUES('Prison');
+INSERT INTO Gangs VALUES('Forest');
+
+INSERT INTO Pillagers VALUES(1, 'Prison', 'Armourer');
+INSERT INTO Pillagers VALUES(2, 'Village', 'Farmer');
+INSERT INTO Pillagers VALUES(3, 'Mountain', 'Fisherman');
+INSERT INTO Pillagers VALUES(4, 'Prison', 'Cartographer');
+INSERT INTO Pillagers VALUES(5, 'Mountain', 'Leatherworker');
+
+INSERT INTO Animal VALUES(1, 'Hunger', 1);
+INSERT INTO Animal VALUES(2, 'Death', 2);
+INSERT INTO Animal VALUES(3, 'Normal', 3);
+INSERT INTO Animal VALUES(4, 'Hunger', 4);
+INSERT INTO Animal VALUES(5, 'Death', 2);
--- docker/init/sql/init-schema.sql
+++ docker/init/sql/init-schema.sql
@@ -1,5 +1,174 @@
-create table user(
-    id int,
-    name text,
-    primary key (id)
-) ;
+CREATE TABLE User(
+    ID INT NOT NULL,
+    Name VARCHAR(20) NOT NULL,
+    CoordinateX INT NOT NULL,
+    CoordinateY INT NOT NULL,
+    CoordinateZ INT NOT NULL,
+    Food_Bar INT,
+    Health VARCHAR(20),
+    Spawn_PointX INT NOT NULL,
+    Spawn_PointY INT NOT NULL,
+    Spawn_PointZ INT NOT NULL,
+    PRIMARY KEY(ID)
+);
+
+CREATE TABLE Chunks(
+   CoordinateX INT NOT NULL,
+   CoordinateY INT NOT NULL,
+   CoordinateZ INT NOT NULL,
+   Weather VARCHAR(20) NOT NULL,
+   PRIMARY KEY (CoordinateX, CoordinateY, CoordinateZ)
+);
+
+
+CREATE TABLE Block_Types(
+   Type VARCHAR(20),
+   Toughness INT,
+   State VARCHAR(20) NOT NULL,
+   PRIMARY KEY(Type)
+);
+
+CREATE TABLE Blocks(
+   CoordinateBlockX INT NOT NULL,
+   CoordinateBlockY INT NOT NULL,
+   CoordinateBlockZ INT NOT NULL,
+   CoordinateChunkX INT NOT NULL,
+   CoordinateChunkY INT NOT NULL,
+   CoordinateChunkZ INT NOT NULL,
+
+   Type VARCHAR(20),
+
+   PRIMARY KEY(CoordinateBlockX, CoordinateBlockY,CoordinateBlockZ ,CoordinateChunkX,CoordinateChunkY,CoordinateChunkZ, Type),
+   FOREIGN KEY(CoordinateChunkX,CoordinateChunkY,CoordinateChunkZ ) REFERENCES Chunks(CoordinateX,CoordinateY,CoordinateZ),
+   FOREIGN KEY(Type) REFERENCES Block_Types(Type)
+);
+
+CREATE TABLE Items(
+   ID INT AUTO_INCREMENT,
+   Type VARCHAR(20) UNIQUE,
+   PRIMARY KEY(ID)
+);
+
+CREATE TABLE Tools(
+   ID INT,
+   Durability INT NOT NULL,
+   PRIMARY KEY(ID),
+   FOREIGN KEY(ID) REFERENCES Items(ID)
+);
+
+CREATE TABLE Enchantments(
+   Type VARCHAR(20),
+   Level INT,
+   PRIMARY KEY(Type, Level)
+);
+
+CREATE TABLE AppliedTo(
+   Type VARCHAR(20),
+   ID INT,
+   Level INT,
+   PRIMARY KEY(Type, Level, ID),
+   FOREIGN KEY(Type, Level) REFERENCES Enchantments(Type,Level),
+   FOREIGN KEY(ID) REFERENCES Items(ID)
+);
+
+
+CREATE TABLE Villages(
+   CoordinateX INT NOT NULL,
+   CoordinateY INT NOT NULL,
+   CoordinateZ INT NOT NULL,
+   PRIMARY KEY(CoordinateX, CoordinateY, CoordinateZ)
+);
+
+CREATE TABLE Villagers(
+    ID INT,
+    CoordinateX INT NOT NULL,
+    CoordinateY INT NOT NULL,
+    CoordinateZ INT NOT NULL,
+    Health VARCHAR(20) NOT NULL,
+    Occupation VARCHAR(20) NOT NULL,
+    PRIMARY KEY(ID),
+    FOREIGN KEY (CoordinateX,CoordinateY,CoordinateZ) REFERENCES Villages(CoordinateX,CoordinateY,CoordinateZ)
+);
+
+
+CREATE TABLE Host_In(
+  Reputation VARCHAR(20),
+  CoordinateX INT NOT NULL,
+  CoordinateY INT NOT NULL,
+  CoordinateZ INT NOT NULL,
+  User_ID INT NOT NULL,
+  PRIMARY KEY(CoordinateX, CoordinateY,CoordinateZ,User_ID),
+  FOREIGN KEY(CoordinateX, CoordinateY,CoordinateZ) REFERENCES Villages(CoordinateX, CoordinateY,CoordinateZ),
+  FOREIGN KEY(User_ID) REFERENCES User(ID)
+);
+
+CREATE TABLE Storage(
+  ID INT AUTO_INCREMENT,
+  Type VARCHAR(20) NOT NULL,
+  PRIMARY KEY(ID)
+);
+
+CREATE TABLE Items_stored_at(
+  CountItems INT,
+  Storage_ID INT,
+  Items_ID INT,
+  PRIMARY KEY(Storage_ID, Items_ID),
+  FOREIGN KEY (Storage_ID) REFERENCES Storage(ID),
+  FOREIGN KEY (Items_ID) REFERENCES Items (ID)
+);
+
+CREATE TABLE Blocks_stored_at(
+  Count INT,
+  Storage_ID INT,
+  Block_Type VARCHAR(20),
+  PRIMARY KEY(Storage_ID, Block_Type),
+  FOREIGN KEY(Storage_ID) REFERENCES Storage(ID),
+  FOREIGN KEY(Block_Type) REFERENCES Block_Types(Type)
+);
+CREATE TABLE Chest(
+  Storage_ID INT NOT NULL,
+  LocationX INT,
+  LocationY INT,
+  LocationZ INT,
+  PRIMARY KEY(Storage_ID)
+);
+
+CREATE TABLE Ender_Chest(
+  Storage_ID INT NOT NULL,
+  User_ID INT,
+  PRIMARY KEY(Storage_ID)
+);
+
+CREATE TABLE User_storage(
+  Storage_ID INT NOT NULL,
+  User_ID INT,
+  PRIMARY KEY(Storage_ID),
+  FOREIGN KEY(User_ID) REFERENCES User(ID)
+);
+
+CREATE TABLE Monster(
+   ID INT,
+   Health VARCHAR(20) NOT NULL,
+   Dmage INT,
+   PRIMARY KEY(ID)
+);
+
+CREATE TABLE Gangs(
+   Location VARCHAR(10),
+   PRIMARY KEY (Location)
+);
+
+CREATE TABLE Pillagers(
+   ID INT NOT NULL,
+   Location VARCHAR(10),
+   Occupation VARCHAR(20),
+   PRIMARY KEY(ID),
+   FOREIGN KEY (Location) REFERENCES Gangs(Location)
+);
+
+CREATE TABLE Animal(
+    ID INT NOT NULL,
+    Health VARCHAR(20) NOT NULL,
+    Damage INT NOT NULL,
+    PRIMARY KEY(ID)
+);
